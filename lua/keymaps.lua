e2001f74a904f46b4de551e941bd44492061ac79
zeyu <zeyu203@qq.com>
2023-07-14 18:54:33 +0800

    reorganize configs

---
+++ lua/keymaps.lua
@@ -0,0 +1,28 @@
+-- [[ Basic Keymaps ]]
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+
+vim.keymap.set('n', 'FF', 'gT')
+vim.keymap.set('n', 'ff', 'gt')
+vim.keymap.set('n', '#', '#N')
+vim.keymap.set('n', 'zl', 'zt')
+vim.keymap.set('n', '：', ':')
+vim.keymap.set('n', '<F8>', ':vsplit<CR>:terminal<CR>:set nonu<CR>i')
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- move
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
---
+++ lua/options.lua
@@ -0,0 +1,65 @@
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.encoding = "utf-8"
+
+-- Set highlight on search
+vim.o.hlsearch = true
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- The amount of space on screen a Tab character can occupy
+vim.opt.tabstop = 4
+
+-- Amount of characters Neovim will use to indent a line
+vim.opt.shiftwidth = 4
+
+-- Controls whether or not Neovim should transform a Tab character to spaces. 
+vim.opt.expandtab = true
+
+-- open new vertical split bottom
+vim.opt.splitbelow = true
+
+-- open new horizontal splits right
+vim.opt.splitright = true
+
+-- don't need the "-- INSERT --" mode hints
+vim.opt.showmode = false
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
