5fd966d5c2bf4e3756ef8ea4310a77cef71a2859
zeyu <zeyu203@qq.com>
2023-07-16 11:31:02 +0800

    add lewis6991/gitsigns.nvim and tpope/vim-sleuth and configure tpope/vim-rhubarb

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,6 +25,29 @@ require('lazy').setup({
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
+  {
+    -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+      end,
+    },
+  },
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
 
@@ -78,6 +101,9 @@ require('lazy').setup({
     },
   },
 }, {})
+
+-- [[ Configure tpope/vim-rhubarb ]]
+vim.g.github_enterprise_urls = {'https://git.intra.weibo.com', 'https://github.com'}
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
